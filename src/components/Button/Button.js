70b5f06ce47df1188614aa0fc37ba3449e0e14cf
YlianaLisova <lisova229@gmail.com>
2022-04-20 18:47:38 +0300

    lecture5_part1

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,11 @@
 import css from './Button.module.css'
-import {Link} from "react-router-dom";
+import {Link, useSearchParams} from "react-router-dom";
 export const Button = ({to,state,children, ...arg}) => {
+    const [query] = useSearchParams();
     return (
-        <Link to={to} state={state}>
+        <Link to={query?`${to}?${query}`:to} state={state}>
             <button className={css.button} {...arg}>{children}</button>
         </Link>
     );
-};
+}
 
