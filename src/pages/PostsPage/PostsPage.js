a2c58c04e715e228d41bee0d2ac4fa37d91e1ee6
YlianaLisova <lisova229@gmail.com>
2022-04-20 18:27:08 +0300

    lecture5_part1

--- src/pages/PostsPage/PostsPage.js
+++ src/pages/PostsPage/PostsPage.js
@@ -1,23 +1,40 @@
 import React, {useEffect, useState} from 'react';
 import {postService} from "../../services";
 import {Post} from "../../components";
-import {Outlet, useParams} from "react-router-dom";
+import {Outlet, useParams, useSearchParams} from "react-router-dom";
 
 
 export const PostsPage = () => {
     const [posts, setPosts] = useState([]);
     const {userId} = useParams();
+    const [query, setQuery] = useSearchParams({page:'1'});
+
 
     useEffect(()=> {
         if(userId){
             postService.getUserById(userId).then(({data})=> setPosts(data))
         } else {
-        postService.getAll().then(({data})=> setPosts(data))
+        postService.getAll(query.get('page'), 10).then(({data})=> setPosts(data))
         }
-    },[userId])
+    },[userId,query])
+
+    const nextPage = () => {
+      // let page = query.get('page');
+      // page = +page +1
+      // setQuery({page:page.toString()})
+
+        const queryObj = Object.fromEntries(query.entries());
+        queryObj.page++
+        setQuery(queryObj)
+
+    }
+
     return (
         <div style={{display:"flex"}}>
-            <div>{posts.map(post => <Post key={post.id} post={post} flag={!userId}/>)}</div>
+            <div>
+                {posts.map(post => <Post key={post.id} post={post} flag={!userId}/>)}
+                <button onClick={()=> nextPage()}>next</button>
+            </div>
             <div><Outlet/></div>
         </div>
     );
