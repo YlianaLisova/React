5345df809664390da8ec3d4fa58cab2176d651d3
YlianaLisova <lisova229@gmail.com>
2022-04-22 23:42:35 +0300

    part 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import './App.css';
+import {useReducer} from "react";
+
+const init = (initCount) => {
+    return {count1: initCount, count2: initCount}
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'inc':
+            return {...state, count1: state.count1 + 1}
+        case 'dec':
+            return {...state, count1: state.count1 - 1}
+        case 'reset':
+            return {...state, count1:0}
+        case 'set':
+            return {...state, count1:action.payload}
+    }
+}
 
 function App() {
-  return (
-    <div>
-
-    </div>
-  );
+    const [state, dispatch] = useReducer(reducer, 0, init);
+    const inc = () => {
+        dispatch({type:'inc'})
+    }
+    const dec = () => {
+      dispatch({type:'dec'})
+    }
+    const reset = () => {
+      dispatch({type:'reset'})
+    }
+    const set = () => {
+      dispatch({type:'set', payload:10})
+    }
+    return (
+        <div>
+            <div>
+                {state.count1}
+                <button onClick={() => inc()}>inc</button>
+                <button onClick={() => dec()}>dec</button>
+                <button onClick={() => reset()}>reset</button>
+                <button onClick={() => set()}>setTo10</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
