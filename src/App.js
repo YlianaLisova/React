c1334887b64118c8327bb1d0886cde87a0133a38
YlianaLisova <lisova229@gmail.com>
2022-04-23 15:03:57 +0300

    part 2

--- src/App.js
+++ src/App.js
@@ -1,48 +1,46 @@
-import './App.css';
-import {useReducer} from "react";
+import React, {useReducer} from 'react';
 
-const init = (initCount) => {
-    return {count1: initCount, count2: initCount}
+const init = (initArg) => {
+  return {count1: initArg}
 }
-
-const reducer = (state, action) => {
-    switch (action.type) {
-        case 'inc':
-            return {...state, count1: state.count1 + 1}
-        case 'dec':
-            return {...state, count1: state.count1 - 1}
-        case 'reset':
-            return {...state, count1:0}
-        case 'set':
-            return {...state, count1:action.payload}
-    }
+const reducer = (state,action) => {
+  switch (action.type) {
+      case 'inc':
+          return {...state, count1: state.count1 + 1}
+      case 'dec':
+          return {...state, count1: state.count1 - 1}
+      case 'reset':
+          return {...state, count1: 0}
+      case 'set':
+          return {...state, count1: action.payload}
+      default:
+          return {state}
+  }
 }
+export const App = () => {
+    const [state,dispatch] = useReducer(reducer, 0, init);
 
-function App() {
-    const [state, dispatch] = useReducer(reducer, 0, init);
     const inc = () => {
-        dispatch({type:'inc'})
+      dispatch({type:'inc'})
     }
     const dec = () => {
-      dispatch({type:'dec'})
+      dispatch({type: 'dec'})
     }
     const reset = () => {
       dispatch({type:'reset'})
     }
     const set = () => {
-      dispatch({type:'set', payload:10})
+      dispatch({type: 'set', payload:10})
     }
+
     return (
         <div>
-            <div>
-                {state.count1}
-                <button onClick={() => inc()}>inc</button>
-                <button onClick={() => dec()}>dec</button>
-                <button onClick={() => reset()}>reset</button>
-                <button onClick={() => set()}>setTo10</button>
-            </div>
+            <p>{state.count1}</p>
+            <button onClick={()=> inc()}>inc</button>
+            <button onClick={()=> dec()}>dec</button>
+            <button onClick={()=> reset()}>reset</button>
+            <button onClick={()=> set()}>setTo10</button>
         </div>
     );
-}
+};
 
-export default App;
