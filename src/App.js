a6bdaabf1d3907bdcfd9f5c4962dfcefae44627b
YlianaLisova <lisova229@gmail.com>
2022-04-27 00:52:45 +0300

    lecture redux base part 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
-  return (
-    <div>
+    const state = useSelector(state => state);
+    
+    const dispatch = useDispatch();
+    return (
+        <div>
+            <h1>state is - {state}</h1>
+            <button onClick={() => {
+                dispatch({type: 'INC', payload: 1})
+            }}>inc
+            </button>
+            <button onClick={() => {
+                dispatch({type: 'DEC', payload: 1})
 
-    </div>
-  );
+            }}>dec
+            </button>
+            <button onClick={() => {
+                dispatch({type: 'RESET', payload: 0})
+
+            }}>reset
+            </button>
+        </div>
+    );
 }
 
 export default App;
